0470ecbd180f9fa3d806d21219edd366b8f0cdaa
NDABOROHEYE Révérien <revendab86@gmail.com>
2022-09-06 15:13:03 +0200

    initial commit

---
+++ src/main/java/ma/enset/Mail.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.enset;
+
+import java.io.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author reverien.ndaboroheye
+ */
+public class Mail {
+  public static void main(String[] args) {
+        System.out.println("reverien.ndaboroheye@obr.gov.bi");
+    }
+}
